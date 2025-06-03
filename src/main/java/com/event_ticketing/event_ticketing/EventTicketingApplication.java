7ce0c596f3858e86d4f50115a0debca8948d2171
Wentao Yang <wyangwork9966@gmail.com>
2025-06-03 16:15:46 -0400

    initial stage of event ticketing platform backend

---
+++ src/main/java/com/event_ticketing/event_ticketing/EventTicketingApplication.java
@@ -0,0 +1,13 @@
+package com.event_ticketing.event_ticketing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventTicketingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventTicketingApplication.class, args);
+	}
+
+}
